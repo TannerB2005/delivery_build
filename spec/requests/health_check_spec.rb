bc4a46ac76cc0bbf703f12adc44d103f3814deb1
Tanner B <sonictanpat@gmail.com>
2025-08-06 16:41:54 -0500

    test added to past run test (hopefully)

---
+++ spec/requests/health_check_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Health Check" do
+  describe "GET /up" do
+    it "returns a success response" do
+      get "/up"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
